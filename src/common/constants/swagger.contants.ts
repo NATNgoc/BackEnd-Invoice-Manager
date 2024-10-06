f2c948e796c430d35ad17f6df64ecdf93772edcd
NATNgoc <nguyenatn2003@gmail.com>
2024-10-06 22:06:06 +0700

    feat: Config base for swagger

---
+++ src/common/constants/swagger.contants.ts
@@ -0,0 +1,11 @@
+export class SwaggerConstant {
+    static readonly api_documentation_credentials = {
+      name: 'admin',
+      pass: 'admin',
+    };
+    static readonly projectName = 'Invoices Management';
+    static readonly description = '## The Invoices Management API description';
+    static readonly version = '1.0';
+  }
+  // Path: packages/backend/src/common/constants/swagger.constant.ts
+  
